8f0ab909c3867d8585c834062e5f1510dae7c437
Kamran <88619404+CarettaCaretta11@users.noreply.github.com>
2024-04-24 15:21:49 +0900

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -4,7 +4,7 @@ const config = {
   github: {
     username: 'CarettaCaretta11', // Your GitHub org/user name. (Required)
     sortBy: 'updated', // stars | updated
-    limit: 5, // How many projects to display.
+    limit: 8, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: ['gitprofile', 'CarettaCaretta11'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
